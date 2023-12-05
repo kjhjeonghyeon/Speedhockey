25bf027a041e276b3b91e427cd768b654bb1e5ff
kjhjeonghyeon <80999210+kjhjeonghyeon@users.noreply.github.com>
2023-12-05 12:24:52 +0900

    복원

--- Assets/Scripts/MyServer.cs
+++ Assets/Scripts/MyServer.cs
@@ -24,7 +24,6 @@ public class MyServer
         }
         catch (Exception e)
         {
-            
         }
     }
 
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -7,11 +7,13 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     Vector3 movePosition;
-    int speed = 3;
+    public int speed = 3;
+    Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -19,19 +21,20 @@ public class PlayerMove : MonoBehaviour
     {
         InputMove();
 
-     
+
     }
     void InputMove()
     {
         float mouse_X = Input.GetAxis("Mouse X");
         float mouse_Y = Input.GetAxis("Mouse Y");
-
+        
         if ((mouse_X != 0 || mouse_Y != 0) && MyClient.client != null)
         {
-            Debug.Log(MyClient.client + " " + MyClient.client.Connected);
+        Debug.Log(MyClient.client + " " + MyClient.client.Connected);
             movePosition = new Vector3(mouse_X, 0, mouse_Y);
-            transform.position += movePosition * Time.deltaTime * speed;
-            transform.position = new Vector3(transform.position.x, 0.7500051f, transform.position.z);
+            rb.velocity = movePosition.normalized * speed;
+            //Debug.Log(rb.velocity);
+            //transform.position += movePosition * Time.deltaTime * speed;
 
             try
             {
@@ -39,13 +42,13 @@ public class PlayerMove : MonoBehaviour
                 byte[] buf = Encoding.Default.GetBytes("MOVE : " + mouse_X + " : " + mouse_Y);
                 MyClient.client.GetStream().Write(buf, 0, buf.Length);
                 MyClient.client.GetStream().Flush();
-            }
+             }
             catch (Exception e)
             {
                 Debug.LogError("Exception during network write: " + e.Message);
             }
-            //MyClientclient.GetStream().Write(buf, 0, buf.Length);
-        }
+        //MyClientclient.GetStream().Write(buf, 0, buf.Length);
+    }
 
     }
 
